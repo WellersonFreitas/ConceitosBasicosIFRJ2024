012768e35842a3ba9354f7e79ae8608d752a8f77
WellersonFreitas <wfalmeida11@gmail.com>
2024-03-08 11:45:24 -0300

    complementei o codigo

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -7,12 +7,25 @@ int peso = int.Parse(Console.ReadLine());
 Console.WriteLine("Informe sua altura: ");
 float altura = float.Parse(Console.ReadLine());
 
-float imc = peso / (altura *altura);
+float imc = peso / (altura * altura);
 
-Console.WriteLine($"seu peso: {peso}\n sua altura: {altura}\n seu imc: {imc}" );
+Console.WriteLine($"seu peso: {peso}\n sua altura: {altura}\n seu imc: {imc:f4}");
 
 string status = string.Empty;
 
-if (imc <= 16.9);
-status = "muito abaixo do peso";
+if (imc <= 16.9)
+    status = "muito abaixo do peso";
+else if (imc <= 17)
+    status = "abaixo do peso";
+else if (imc <= 18.5)
+    status = "peso normal";
+else if (imc <= 25)
+    status = "acima do peso";
+else if (imc <= 30)
+    status = "obesidade grau 1";
+else if (imc <= 35)
+    status = "obesidade grau 2";
+else if (imc <= 40)
+    status = "obesidade grau 3";
 
+Console.WriteLine(status);
