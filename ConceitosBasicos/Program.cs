8a5f1cf590c5b602eb2ef60dc4413773220a49a0
wfalm <wfalm@DESKTOP-JQVFF8E>
2024-03-08 06:53:49 -0300

    Adicionar arquivos de projeto.

---
+++ ConceitosBasicos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.Design;
+using System.Reflection.PortableExecutable;
+using System.Xml.Serialization;
+
+Console.WriteLine("Informe seu peso: ");
+int peso = int.Parse(Console.ReadLine());
+Console.WriteLine("Informe sua altura: ");
+float altura = float.Parse(Console.ReadLine());
+
+float imc = peso / (altura *altura);
+
+Console.WriteLine($"seu peso: {peso}\n sua altura: {altura}\n seu imc: {imc}" );
+
+string status = string.Empty;
+
+if (imc <= 16.9);
+status = "muito abaixo do peso";
+
